ecea132c2cb4bcf5937c11efc43ab93155912a3c
Anthonny Tineo Ruiz <anthonnytineo@MacBook-Pro-de-Anthonny.local>
2021-05-21 11:24:00 -0400

    practica de useeffect con react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import React, { ussState, useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [contador, setContador] = useState(0);
+  const [tareas, settareas] = useState([]);
+
+  const incrementar = () => {
+    setContador(prevContador => prevContador + 1);
+  }
+
+  const incrementarTareas = () => {
+    settareas(["Nueva tarea", ...tareas]);
+  }
+
+// componente para montar
+  useEffect(() => {
+    cargarListaPaises();
+  }, [])
+
+  useEffect(() => {
+    console.log("Nueva tarea ha sido añadida")
+  }, [tareas])
+
+//llamada a una API externa
+const cargarListaPaises = async () => {
+  const data = await fetch('https://covid19.mathdro.id/api/countries');
+  const paises = await data.json();
+
+}
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {contador}
+      <button onClick={incrementar}>Sumar</button>
+      <button onClick={incrementarTareas}>Añadir Tarea</button>
     </div>
   );
 }
